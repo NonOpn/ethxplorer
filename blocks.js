931ee8f2ef97a3b998a277e322b2814086a4f908
Kevin Le Perf <codlabtech@gmail.com>
2017-09-15 22:09:24 +0200

    simplify blocks and use callback instead of getBlock promise > fix a bug

--- blocks.js
+++ blocks.js
@@ -70,38 +70,25 @@ Blocks.prototype.internalStart = function(first_block) {
 
 Blocks.prototype.fetchBlock = function(block_number, end) {
   return new Promise((resolve, reject) => {
-    try{
-      if(block_number < end) {
-        this.start_time = process.hrtime();
-        console.log(`start  current ${block_number} tx`);
-        var block_promise = web3.eth.getBlock(block_number, true);
-        block_promise.then(block => {
-          if (block != null && block.transactions != null) {
-            console.log(`start  current ${block_number} tx == #${block.transactions.length} tx`);
-            block.transactions.forEach(transaction => {
-              ethereum_address_tx.save(transaction)
-              .then((tx) => {
-              })
-              .catch((e) => {
-                console.log(e);
-              })
-            });
-            diff = process.hrtime(this.start_time);
-            console.log(`finish current ${block_number} tx == #${block.transactions.length} ${diff}`);
-          } else {
-            console.log(`finish current block == no transaction`);
-          }
-          resolve(true);
-        })
-        .catch((e) => {
-          reject(e);
+    this.start_time = process.hrtime();
+    var block_promise = web3.eth.getBlock(block_number, true, (err, block) => {
+      if (block != null && block.transactions != null) {
+        block.transactions.forEach(transaction => {
+          ethereum_address_tx.save(transaction)
+          .then((tx) => {
+          })
+          .catch((e) => {
+            console.log(e);
+          })
         });
+        diff = process.hrtime(this.start_time);
+        console.log(`finish current ${block_number} tx == #${block.transactions.length} ${diff}`);
       } else {
-        resolve(false);
+        if(err) console.log(err);
+        console.log(`finish current block == no transaction`);
       }
-    }catch(err) {
-      reject(err);
-    }
+      resolve(true);
+    });
   });
 }
 
@@ -110,14 +97,16 @@ Blocks.prototype.manageTransactionsForBlocks = function(startBlockNumber, endBlo
     console.log(`from #${startBlockNumber} to #${endBlockNumber}`);
     const call = () => {
       if(startBlockNumber < endBlockNumber) {
+        console.log(1);
         var promises = [];
         const treshold = startBlockNumber + this._speedup;
 
         while(startBlockNumber < endBlockNumber && startBlockNumber < treshold) {
-          promises.push(this.fetchBlock(startBlockNumber, endBlockNumber))
+          promises.push(this.fetchBlock(startBlockNumber))
           startBlockNumber++;
         }
 
+        console.log(promises.length);
         Promise.all(promises)
         .then(result => {
           this._save_timer -= promises.length;
@@ -132,6 +121,7 @@ Blocks.prototype.manageTransactionsForBlocks = function(startBlockNumber, endBlo
           console.log(err);
         })
       } else {
+        console.log("finished");
         resolve(false);
       }
     }
