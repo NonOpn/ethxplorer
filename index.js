dc343c0352e814b95d9bd718b5c30278197fe50e
Kevin Le Perf <codlabtech@gmail.com>
2017-11-26 10:19:00 +0100

    wait for 10s at startup

--- index.js
+++ index.js
@@ -3,13 +3,15 @@
 const DatabaseConnection = require("./database/init");
 const Blocks = require("./blocks.js");
 const Server = require("./server.js");
+const Wait = require("./wait.js");
 
 const Provider = require("./provider/etherscan_provider");
 
 const blocks = new Blocks(new Provider());
 const server = new Server();
 
-DatabaseConnection.init()
+Wait.wait(10)
+.then(() => { return DatabaseConnection.init() })
 .then(results => {
   console.log("database opened");
   blocks.start(5);
