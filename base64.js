9f63188461c89ce88f16855da5d97154c57fb91d
Kevin Le Perf <codlabtech@gmail.com>
2017-11-27 22:50:46 +0100

    add base64 wards and few logs

--- base64.js
+++ base64.js
@@ -1,10 +1,20 @@
 module.exports = {
-  from0x: function(string) {
-    string = string.replace("0x","").toLowerCase();
-    return Buffer.from(string, "hex").toString("base64");
+  from0x: function(string, debug_tx, debug_block_number) {
+    const replaced = string.replace("0x","").toLowerCase();
+    if(replaced.length > 0) {
+      const result = Buffer.from(replaced, "hex").toString("base64");
+
+      console.error(string.length+" "+result.length+" "+debug_tx+" "+debug_block_number);
+      return result;
+    }
+
+    return "";
   },
   to0x: function(base64) {
-    const string = Buffer.from(base64, "base64").toString("hex");
-    return "0x" + string.toLowerCase();
+    if(base64.length > 0) {
+      const string = Buffer.from(base64, "base64").toString("hex");
+      return "0x" + string.toLowerCase();
+    }
+    return "0x";
   }
 }
