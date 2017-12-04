32adf59f2e5da163971f3e22e8b3cbbd2ee9241c
Kevin Le Perf <codlabtech@gmail.com>
2017-12-04 02:07:23 +0100

    add mqtt and update providers

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ const DatabaseConnection = require("./database/init");
 const Blocks = require("./blocks.js");
 const Server = require("./server.js");
 const Wait = require("./wait.js");
+const MQTT = require("./mqtt.js");
 
-const Provider = require("./provider/etherscan_provider");
+const Provider = require("./provider/json_rpc_provider");
+//const Provider = require("./provider/etherscan_provider");
+//const Provider = require("./provider/web3_provider");
 
-const blocks = new Blocks(new Provider());
+const mqtt = new MQTT();
+const blocks = new Blocks(new Provider(), mqtt);
 const server = new Server();
 
 Wait.wait(10)
 .then(() => { return DatabaseConnection.init() })
 .then(results => {
   console.log("database opened");
+  mqtt.start();
   blocks.start(5);
 
   server.start();
---
+++ mqtt.js
@@ -0,0 +1,61 @@
+var util = require("util"),
+mosca = require('mosca'),
+EventEmitter = require("events").EventEmitter;
+
+var moscaSettings = {
+  backend: {},
+  interfaces: [{
+    type: "mqtt",
+    port: 8883,
+  }]
+};
+
+function setup() {
+  console.log("The MQTT server is running")
+}
+
+module.exports = new EventEmitter();
+
+var MQTT = function() {
+  this.server = undefined;
+}
+
+MQTT.prototype.start = function() {
+  if(this.server == undefined) {
+    this.server = new mosca.Server(moscaSettings);
+
+    //server.authenticate = authenticate_clients.authenticate;
+    //server.authorizePublish = authenticate_clients.authorizePublish;
+    //server.authorizeSubscribe = authenticate_clients.authorizeSubscribe;
+
+    this.server.on('ready', setup);
+
+    this.server.on('clientConnected', (client) => {
+      console.log('client connected', client.id);
+    });
+
+    this.server.on("subscribed", (packet, client) => {
+      this.emit("client/connect", client);
+    })
+
+    this.server.on("clientDisconnected", (packet, client) => {
+      this.emit("client/disconnect", packet);
+    });
+  }
+}
+
+MQTT.prototype.publish = function(topic, json) {
+  if(this.server != undefined) {
+    this.server.publish({
+      topic: topic,
+      payload: JSON.stringify(json),
+      qos:0,
+      retain: false
+    }, function() {
+    });
+  }
+}
+
+util.inherits(MQTT, EventEmitter);
+
+module.exports = MQTT;
